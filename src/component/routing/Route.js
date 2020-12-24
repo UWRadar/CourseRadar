0d6a870c0a244b973be6a1d662a39e5c34781f62
Weifeng Li <didntpay@Weifengs-MacBook-Pro.local>
2020-12-24 00:49:37 -0800

    initial commit

---
+++ src/component/routing/Route.js
@@ -0,0 +1,49 @@
+import { Home } from '@material-ui/icons';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import HomePage from "../home/HomePage"
+export default class Routing extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            id: 'info',
+            loggedIn: false,
+            loggedInUser: "",
+            usingIp: false,
+            openLoginWindow: false,
+            width: 0,
+            reinitializeDraw: false,
+        }
+    }
+
+    render() {
+        return (
+            <Router>
+                <div className="App">
+                    <h2>Empty routing page</h2>
+                    <Switch>
+                        <Route path="/" exact component={HomePage} />
+                        <Route path="/profile/:username" component={HomePage} />
+                        <Route path="/SearchResult" component={HomePage} />
+                        <Route path="/CourseDetail/:courseName" component={HomePage}/>
+                    </Switch>
+                </div>
+            </Router>
+            
+        );
+    }
+
+    
+
+}
+
+function checkStatus(response) { 
+    if ((response.status >= 200 && response.status < 300) || response.status === 0) {  
+        console.log(5)
+        return response.text();
+    } else { 
+        console.log(5) 
+        return Promise.reject(new Error(response.status + ": " + response.statusText)); 
+    } 
+}
--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Routing from './component/routing/Route'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Routing />
   </React.StrictMode>,
   document.getElementById('root')
 );
