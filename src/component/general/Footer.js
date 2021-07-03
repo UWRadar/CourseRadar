aeb976968c21c4a165f73037b06838f411896bc3
Lingxiao Yang <63026870+yang029-1937011@users.noreply.github.com>
2021-07-03 16:28:11 +0800

    uniform layout for the icons

--- src/component/general/Footer.js
+++ src/component/general/Footer.js
@@ -8,14 +8,14 @@ export default function Footer() {
         <footer id = 'nav'>
             <img id = 'footer-img-for-nav' src="../img/original4.png" className="web-logo" alt="Logo for Course Radar" />
             <div id='logos'>
-                <Popup trigger={<img src='../img/Wechat-logo.jpg' alt="logo for Wechat"></img>} position="left middle" offsetX="10" arrow="true">
+                <Popup trigger={<img src='../img/Wechat-logo.svg' alt="logo for Wechat"></img>} position="left middle" offsetX="10" arrow="true">
                     <img src="../img/QR_code.jpg" alt="logo for QRcode" id="QRcode"></img>
                     <p>微信扫码</p>
                 </Popup>
-                <a href="https://weibo.com/u/7372059969"><img src='../img/Weibo-logo.png' alt="logo for Weibo"></img> </a>
-                <a href="https://space.bilibili.com/494538320" target="_blank"> <img src='../img/bilibili-logo.jpg' alt="logo for bilibili"></img> </a>
-                <a href="https://www.zhihu.com/people/hua-da-ke-you" target="_blank"><img src='../img/zhihu-logo.png' alt="logo for zhihu"></img> </a>
-                <a><img src='../img/mail-logo.png' alt="logo for mail"></img> </a>
+                <a href="https://weibo.com/u/7372059969"><img src='../img/Weibo-logo.svg' target="_blank" alt="logo for Weibo"></img> </a>
+                <a href="https://space.bilibili.com/494538320" target="_blank"> <img src='../img/bilibili-logo.svg' alt="logo for bilibili"></img> </a>
+                <a href="https://www.zhihu.com/people/hua-da-ke-you" target="_blank"><img src='../img/zhihu-logo.svg' alt="logo for zhihu"></img> </a>
+                <a href="mailto:ohcmuw@gmail.com"><img src='../img/mail-logo.svg' alt="logo for mail"></img> </a>
             </div>
             {/* <div id="home" className='footer-active'>
                     <img src="../img/home-active.png" alt="Logo for home" />
