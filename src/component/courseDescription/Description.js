4c7a28db7417db4a4951edef2804467440546dda
Lingxiao Yang <63026870+yang029-1937011@users.noreply.github.com>
2021-08-09 10:50:05 +0800

    fix bugs

--- src/component/courseDescription/Description.js
+++ src/component/courseDescription/Description.js
@@ -6,44 +6,55 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons'
 import background from "../../img/guthrie.jpeg";
 
+
 const Description = (props) => {
     console.log(props);
+
+    const handleClick = () => {
+        window.location.href = "/"
+    }
+
     return (
-        <div className="description">
-            <div className="hero" style={{backgroundImage: `url(${background})`}}>
-                <div className="overlay">
-                    <Container>
-                        <Row>
-                            <Col>
-                                <p className="courseCode">{props.courseName}</p>
-                                <p className="courseName">{props.courseItems.courseFullName}</p>
-                                <a href={props.courseItems.myplanLink} className="myPlan"><FontAwesomeIcon icon={faExternalLinkAlt} aria-label="link" /> MyPlan</a>
-                            </Col>
-                            <Col>
-                                <p className="courseCredit">{props.courseItems.credit}</p>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col>
-                                <p className="courseDesc">{props.courseItems.description}</p>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col>
-                                {props.courseItems.creditType.split("/").map(element => {
-                                return (<div ><div className="button type">{element}</div>{' '}</div>);})}
-                            </Col>
-                        </Row>
-                    </Container>
-                    <br></br>
+        <div>
+            <div className="description">
+                <div className="hero" style={{backgroundImage: `url(${background})`}}>
+                    <div className="overlay">
+                        <Container>
+                            <Row>
+                                <Col>
+                                    <p className="courseCode">{props.courseName}</p>
+                                    <p className="courseName">{props.courseItems.courseFullName}</p>
+                                    <a href={props.courseItems.myplanLink} className="myPlan"><FontAwesomeIcon icon={faExternalLinkAlt} aria-label="link" /> MyPlan</a>
+                                </Col>
+                                <Col>
+                                    <p className="courseCredit">{props.courseItems.credit}</p>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col>
+                                    <p className="courseDesc">{props.courseItems.description}</p>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col>
+                                    {props.courseItems.creditType.split("/").map(element => {
+                                    return (<div ><div className="button type">{element}</div>{' '}</div>);})}
+                                </Col>
+                            </Row>
+                        </Container>
+                        <br></br>
+                    </div>
                 </div>
+                <StarSmall
+                    diffRating={props.courseItems.difficulty}
+                    workRating={props.courseItems.workload}
+                    avgRating={props.courseItems.averageGPA}
+                />
+
             </div>
-            <StarSmall
-                diffRating={props.courseItems.difficulty}
-                workRating={props.courseItems.workload}
-                avgRating={props.courseItems.averageGPA}
-            />
+            
         </div>
+
 
     )
 }
--- src/component/courseDescription/StarLarge.js
+++ src/component/courseDescription/StarLarge.js
@@ -5,9 +5,12 @@ import { Container, Row, Col } from 'reactstrap';
 import flag from '../../img/flag.svg';
 import clock from '../../img/clock.svg';
 import award from '../../img/award.svg';
-
+import Button from '@material-ui/core/Button';
 const StarLarge = (props) => {
     console.log(props)
+    const handleClick = () => {
+        window.location.href = "/survey"
+    }
     return (
         <Container className='ratingstar'> 
         <Row>
@@ -17,8 +20,11 @@ const StarLarge = (props) => {
                         <Row xs="2"><Col className="cat"><img src={flag} className="icon" alt="flag" /> <span> </span>Difficulty<span> </span>: </Col><Col className='stars'><StarRatings classname='large' rating={props.diffRating} starDimension="16px" starRatedColor="rgb(230, 178, 84)" starSpacing="0.7px"/></Col></Row>
                         <Row xs="2"><Col className="cat"><img src={clock} className="icon" alt="clock" /><span> </span>Workload<span> </span>: </Col><Col className='stars'><StarRatings classname='large' rating={props.workRating} starDimension="16px" starRatedColor="rgb(230, 178, 84)" starSpacing="0.7px"/></Col></Row>
                         <Row xs="2"><Col className="cat"><img src={award} className="icon" alt="award" /><span> </span>Avg.GPA<span> </span>: </Col><Col className='stars'> <StarRatings classname='large' rating={props.avgRating} starDimension="16px" starRatedColor="rgb(230, 178, 84)" starSpacing="0.7px"/></Col></Row>
+                        
                         <br></br>
+                        <Row xs="2"><Button variant="contained" size="large" id="button" onClick={handleClick}>提交更多课评</Button></Row>
                     </Container>
+                    
                 </div>
             </Col>
         </Row>
