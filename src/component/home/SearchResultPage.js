73ec7543ad09b10be6c89395bfaf61652844b01a
David Xie <xdkdavid@gmail.com>
2021-11-29 01:31:55 -0800

    implemented all logic for course level and credit number; ; ;

--- src/component/home/SearchResultPage.js
+++ src/component/home/SearchResultPage.js
@@ -76,71 +76,69 @@ export default function SearchResultPage(props) {
     // Custom data cleaning function to account for mutually exclusive selection
     function handleFilterChange(destination, newValue) {
         console.log("handleFilterChange(" + destination + ", " + newValue);
-        if(destination === "courseLevel") {
+        if (destination === "courseLevel") {
             // Toggling between all and not all
-            if(newValue === "all" && courseLevel.indexOf("all")<0) {
+            if (newValue === "all" && courseLevel.indexOf("all") < 0) {
                 setCourseLevel(["all"]);
             } else if (courseLevel[0] === "all" && newValue !== "all") {
                 setCourseLevel([newValue]);
             }
             // When it's not all, remove value if it's exist, except we do all if we have nothing left
-            else if (newValue !== "all" && courseLevel.indexOf(newValue)>=0) {
+            else if (newValue !== "all" && courseLevel.indexOf(newValue) >= 0) {
                 // ECMA6 method to remove by value (all occurrences) in an array (it makes a copy without modifying original array)
                 const updatedLevel = courseLevel.filter(e => e !== newValue);
-                if(updatedLevel.size === 0) {
+                if (updatedLevel.size === 0) {
                     setCourseLevel(["all"]);
                 } else {
                     setCourseLevel(updatedLevel);
                 }
-            }
-            else if (newValue !== "all" && courseLevel.indexOf(newValue)<0) {
+            } else if (newValue !== "all" && courseLevel.indexOf(newValue) < 0) {
                 // Non Mutative way to concat an array
                 setCourseLevel(courseLevel.concat(newValue));
             }
-        } else if(destination === "creditNumber") {
+        } else if (destination === "creditNumber") {
             // Toggling between all and not all
-            if(newValue==="all" && creditNumber.indexOf("all")<0) {
+            if (newValue === "all" && creditNumber.indexOf("all") < 0) {
                 setCreditNumber(["all"]);
             } else if (creditNumber[0] === "all" && newValue !== "all") {
                 setCreditNumber([newValue]);
             }
             // When it's not all, remove value if it's exist, except we do all if we have nothing left
-            else if (newValue !== "all" && creditNumber.indexOf(newValue)>=0) {
+            else if (newValue !== "all" && creditNumber.indexOf(newValue) >= 0) {
                 // ECMA6 method to remove by value (all occurrences) in an array (it makes a copy without modifying original array)
                 const updatedLevel = creditNumber.filter(e => e !== newValue);
-                if(updatedLevel.size === 0) {
+                if (updatedLevel.size === 0) {
                     setCreditNumber(["all"]);
                 } else {
                     setCreditNumber(updatedLevel);
                 }
-            }
-            else if (newValue !== "all" && creditNumber.indexOf(newValue)<0) {
+            } else if (newValue !== "all" && creditNumber.indexOf(newValue) < 0) {
                 // Non Mutative way to concat an array
                 setCreditNumber(creditNumber.concat(newValue));
             }
-        } else if(destination === "courseType") {
+        } else if (destination === "courseType") {
             // Toggling between all and not all
-            if(newValue==="all" && courseType.indexOf("all")<0) {
+            if (newValue === "all" && courseType.indexOf("all") < 0) {
                 setCourseType(["all"]);
-            } else if (courseType[0]==="all" && newValue !== "all" && newValue !== "None") {
+            } else if (courseType[0] === "all" && newValue !== "all" && newValue !== "None") {
                 setCourseType([newValue]);
             }
             // Toggle between None and not None
-            else if(newValue==="None" && courseType.indexOf("None")<0) {
+            else if (newValue === "None" && courseType.indexOf("None") < 0) {
                 setCourseType(["None"]);
-            } else if (courseType[0]=== "None" && newValue !== "all" && newValue !== "None") {
+            } else if (courseType[0] === "None" && newValue !== "all" && newValue !== "None") {
                 setCourseType([newValue]);
             }
             // When it's not all and not none, remove value if it's exist, except we do all if we have nothing left
-            else if (newValue !== "all" && newValue !== "None" && courseType.indexOf(newValue)>=0) {
+            else if (newValue !== "all" && newValue !== "None" && courseType.indexOf(newValue) >= 0) {
                 // ECMA6 method to remove by value (all occurrences) in an array (it makes a copy without modifying original array)
                 const updatedLevel = courseType.filter(e => e !== newValue);
-                if(updatedLevel.size === 0) {
+                if (updatedLevel.size === 0) {
                     setCourseType(["all"]);
                 } else {
                     setCourseType(updatedLevel);
                 }
-            } else if (newValue !== "all" && newValue !== "None" && courseType.indexOf(newValue)<0) {
+            } else if (newValue !== "all" && newValue !== "None" && courseType.indexOf(newValue) < 0) {
                 // Non Mutative way to concat an array
                 setCourseType(courseType.concat(newValue));
             }
@@ -152,7 +150,8 @@ export default function SearchResultPage(props) {
 
     return (
         <div className="search-result">
-            <SearchFilter courseLevel={courseLevel} creditNumber={creditNumber} courseType={courseType} handleFilterChange={handleFilterChange}/>
+            <SearchFilter courseLevel={courseLevel} creditNumber={creditNumber} courseType={courseType}
+                          handleFilterChange={handleFilterChange}/>
             <div className="course-list2">
             </div>
         </div>
@@ -165,45 +164,37 @@ function SearchFilter(props) {
             <h1>二次筛选</h1>
             {/*<h2>课程级别</h2>*/}
             <FormLabel component="legend">课程级数</FormLabel>
-            <RadioGroup
-                aria-label="gender"
-                name="controlled-radio-buttons-group"
-                value={props.courseLevel.indexOf("all") >= 0 ? "all" : ""}
-                onChange={(value)=>props.handleFilterChange("courseLevel", value)}
-            >
-                <FormControlLabel value="all" control={<Radio/>} label="全部"/>
-                {LEVELS.map((input) => {
-                    return (<FormControlLabel
-                        control={<Checkbox checked={props.courseLevel.indexOf(input) >= 0} onChange={()=>{props.handleFilterChange("courseLevel", input)}}/>}
-                        label={input}/>);
-                })}
-            </RadioGroup>
+            <FormControlLabel control={<Radio checked={props.courseLevel.indexOf("all") >= 0} onChange={() => {props.handleFilterChange("courseLevel", "all")}}/>} label="全部"/>
+            {LEVELS.map((input) => {
+                    return (<div>
+                                <FormControlLabel control={<Checkbox checked={props.courseLevel.indexOf(input) >= 0}
+                                                                     onChange={() => {props.handleFilterChange("courseLevel", input)}}/>}
+                                                  label={input}/>
+                            </div>);
+            })}
             <FormLabel component="legend">学分数量</FormLabel>
-            <RadioGroup
-                aria-label="gender"
-                name="controlled-radio-buttons-group"
-                value={props.creditNumber.indexOf("all") >= 0 ? "all" : ""}
-                onChange={(value)=>props.handleFilterChange("creditNumber", value)}
-            >
-                <FormControlLabel value="all" control={<Radio/>} label="全部"/>
+                <FormControlLabel control={<Radio checked={props.creditNumber.indexOf("all") >= 0} onChange={() => {props.handleFilterChange("creditNumber", "all")}}/>} label="全部"/>
                 {CREDITS.map((input) => {
-                    return (<FormControlLabel
-                        control={<Checkbox checked={props.creditNumber.indexOf(input) >= 0} onChange={()=>{props.handleFilterChange("creditNumber", input)}}/>}
-                        label={input}/>);
+                    return (<div>
+                                <FormControlLabel control={<Checkbox checked={props.creditNumber.indexOf(input) >= 0}
+                                                                     onChange={() => {props.handleFilterChange("creditNumber", input)}}/>}
+                                                  label={input}/>
+                            </div>);
                 })}
-            </RadioGroup>
             <FormLabel component="legend">通识类别</FormLabel>
-                <FormControlLabel control={<Radio checked={props.courseType.indexOf("all") >= 0} onChange={()=>{props.handleFilterChange("courseType", "all")}}/>} label="全部"/>
-                {CREDIT_TYPES.map((input) => {
-                    if(input!=="None") {
-                        return (
-                            <div>
+            <FormControlLabel control={<Radio checked={props.courseType.indexOf("all") >= 0} onChange={() => {props.handleFilterChange("courseType", "all")}}/>} label="全部"/>
+            {CREDIT_TYPES.map((input) => {
+                if (input !== "None") {
+                    return (
+                        <div>
                             <FormControlLabel
-                            control={<Checkbox checked={props.courseType.indexOf(input === "I&S" ? "IS" : input) >= 0} onChange={()=>{props.handleFilterChange("courseType", input === "I&S" ? "IS" : input)}}/>}
-                            label={input}/></div>);
-                    }
-                })}
-                <FormControlLabel control={<Radio checked={props.courseType.indexOf("None") >= 0} onChange={()=>{props.handleFilterChange("courseType", "None")}}/>} label="无通识学分"/>
+                                control={<Checkbox checked={props.courseType.indexOf(input === "I&S" ? "IS" : input) >= 0}
+                                                   onChange={() => {props.handleFilterChange("courseType", input === "I&S" ? "IS" : input)}}/>}
+                                label={input}/>
+                        </div>);
+                }
+            })}
+            <FormControlLabel control={<Radio checked={props.courseType.indexOf("None") >= 0} onChange={() => {props.handleFilterChange("courseType", "None")}}/>} label="无通识学分"/>
         </div>
     )
 }
