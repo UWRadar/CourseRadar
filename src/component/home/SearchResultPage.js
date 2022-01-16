08930df46437a4146738b1a497ff7e6722474cf6
David Xie <xdkdavid@gmail.com>
2022-01-15 23:39:05 -0800

    remove some repeated codes by introduce functions

--- src/component/home/SearchResultPage.js
+++ src/component/home/SearchResultPage.js
@@ -36,50 +36,28 @@ export default function SearchResultPage(props) {
         courseName_init = "all";
     }
 
-    // Extract course_level query parameter
-    let courseLevel_init;
-    if (query.get("course_level") === null) {
-        courseLevel_init = ["all"];
-    } else {
-        courseLevel_init = query.get("course_level").split(".");
-        if (courseLevel_init.includes("all")) {
-            courseLevel_init = ["all"]
-        } else if (!courseLevel_init.every(r => LEVELS.concat(["all"]).includes(r))) {
-            courseLevel_init = ["all"]; // default to all for malformed query
+    // This function will extract course level, credit number, and credit type parameter's value
+    // Default to all if it does not exist or contains unacceptable values
+    function extractParam(queryName, referenceConst, extraAllowedValue = []) {
+        let result;
+        if (query.get(queryName) === null) {
+            result = ["all"];
+        } else {
+            result = query.get(queryName).split(".");
+            if (result.includes("all")) {
+                result = ["all"]
+            } else if (!result.every(r => referenceConst.concat(["all"].concat(extraAllowedValue)).includes(r))) {
+                result = ["all"]; // default to all for malformed query
+            }
         }
-    }
-
-    // Extract credit_number query parameter
-    let creditNumber_init;
-    if (query.get("credit_number") === null) {
-        creditNumber_init = ["all"];
-    } else {
-        creditNumber_init = query.get("credit_number").split(".");
-        if (creditNumber_init.includes("all")) {
-            creditNumber_init = ["all"]
-        } else if (!creditNumber_init.every(r => CREDITS.concat(["all"]).includes(r))) {
-            creditNumber_init = ["all"]; // default to all for malformed query
-        }
-    }
-
-    // Extract credit_type query parameter
-    let courseType_init;
-    if (query.get("course_type") === null) {
-        courseType_init = ["all"];
-    } else {
-        courseType_init = query.get("course_type").split(".");
-        if (courseType_init.includes("all")) {
-            courseType_init = ["all"];
-        } else if (!courseType_init.every(r => CREDIT_TYPES.concat(["all", "IS", "None"]).includes(r))) {
-            courseType_init = ["all"]; // default to all for malformed query
-        }
+        return result;
     }
 
     // Note: need to extract parameter first before I can set states
     const [courseName, setCourseName] = useState(courseName_init);
-    const [courseLevel, setCourseLevel] = useState(courseLevel_init);
-    const [creditNumber, setCreditNumber] = useState(creditNumber_init);
-    const [courseType, setCourseType] = useState(courseType_init);
+    const [courseLevel, setCourseLevel] = useState(extractParam("course_level", LEVELS));
+    const [creditNumber, setCreditNumber] = useState(extractParam("credit_number", CREDITS));
+    const [courseType, setCourseType] = useState(extractParam("course_type", CREDIT_TYPES, ["IS", "None"]));
     const [isParamValid, setIsParamValid] = useState(false); // TODO: Add a non-critical warning banner/pop-up in search result stating that which parameter has invalid value and hence being ignored (instead of throw out an error screen)
     const [loaded, setLoaded] = useState(true);
     const [isCourseDNE, setIsCourseDNE] = useState(false);
@@ -87,100 +65,53 @@ export default function SearchResultPage(props) {
     const [courseCards, setCourseCards] = useState([]);
 
     // Custom data cleaning function to account for mutually exclusive selection
-    function handleFilterChange(destination, newValue) {
+    function handleFilterChange(dest, newVal) {
         //console.log("handleFilterChange(" + destination + ", " + newValue);
         let url = new URL(document.URL);
         let params = new URLSearchParams(url.search);
-        if (destination === "courseLevel") {
+        if (dest === "courseLevel") {
+            changeFilter(courseLevel, newVal, "course_level", setCourseLevel);
+        } else if (dest === "creditNumber") {
+            changeFilter(creditNumber, newVal, "credit_number", setCreditNumber);
+        }else if (dest === "courseType") {
+            changeFilter(courseType, newVal, "course_type", setCourseType, true);
+        }
+        function changeFilter(originalVar, newValue, queryParam, setStateFn, hasNone = false) {
             // ES6 way to copy/clone an array
-            let newCourseLevel = [...courseLevel];
+            let result = [...originalVar];
 
             // Toggling between all and not all
-            if (newValue === "all" && courseLevel.indexOf("all") < 0) {
-                newCourseLevel = ["all"];
-            } else if (courseLevel[0] === "all" && newValue !== "all") {
-                newCourseLevel = [newValue];
+            if (newValue === "all" && originalVar.indexOf("all") < 0) {
+                result = ["all"];
+            } else if (originalVar[0] === "all" && newValue !== "all" && (!hasNone || newValue !== "None")) {
+                result = [newValue];
             }
+
+            // Toggle between None and not None (it's only effective if hasNone is true)
+            else if (!hasNone || (newValue === "None" && courseType.indexOf("None") < 0)) {
+                result = ["None"];
+            } else if (!hasNone || (courseType[0] === "None" && newValue !== "all" && newValue !== "None")) {
+                result = [newValue];
+            }
+
             // When it's not all, remove value if it's exist, except we do all if we have nothing left
-            else if (newValue !== "all" && courseLevel.indexOf(newValue) >= 0) {
+            else if (newValue !== "all" && (!hasNone || newValue !== "None") && originalVar.indexOf(newValue) >= 0) {
                 // ECMA6 method to remove by value (all occurrences) in an array (it makes a copy without modifying original array)
-                const updatedLevel = newCourseLevel.filter(e => e !== newValue);
+                const updatedLevel = result.filter(e => e !== newValue);
                 if (updatedLevel[0] === "" || updatedLevel.length === 0) {
-                    newCourseLevel = ["all"];
+                    result = ["all"];
                 } else {
-                    newCourseLevel = updatedLevel;
+                    result = updatedLevel;
                 }
-            } else if (newValue !== "all" && courseLevel.indexOf(newValue) < 0) {
+            } else if (newValue !== "all" && (!hasNone || newValue !== "None") && originalVar.indexOf(newValue) < 0) {
                 // Non Mutative way to concat an array
-                newCourseLevel = courseLevel.concat(newValue);
+                result = originalVar.concat(newValue);
             }
 
-            params.set('course_level', newCourseLevel.join("."));
+            params.set(queryParam, result.join("."));
             window.history.pushState(null, null, '?' + params.toString());
 
-            setCourseLevel(newCourseLevel);
-        } else if (destination === "creditNumber") {
-            // ES6 way to copy/clone an array
-            let newCreditNumber = [...creditNumber];
-
-            // Toggling between all and not all
-            if (newValue === "all" && creditNumber.indexOf("all") < 0) {
-                newCreditNumber = ["all"];
-            } else if (creditNumber[0] === "all" && newValue !== "all") {
-                newCreditNumber = [newValue];
-            }
-            // When it's not all, remove value if it's exist, except we do all if we have nothing left
-            else if (newValue !== "all" && creditNumber.indexOf(newValue) >= 0) {
-                // ECMA6 method to remove by value (all occurrences) in an array (it makes a copy without modifying original array)
-                const updatedCreditNumber = newCreditNumber.filter(e => e !== newValue);
-                if (updatedCreditNumber[0] === "" || updatedCreditNumber.length === 0) {
-                    newCreditNumber = ["all"];
-                } else {
-                    newCreditNumber = updatedCreditNumber;
-                }
-            } else if (newValue !== "all" && creditNumber.indexOf(newValue) < 0) {
-                // Non Mutative way to concat an array
-                newCreditNumber = creditNumber.concat(newValue);
-            }
-
-            params.set('credit_number', newCreditNumber.join("."));
-            window.history.pushState(null, null, '?' + params.toString());
-
-            setCreditNumber(newCreditNumber);
-        } else if (destination === "courseType") {
-            // ES6 way to copy/clone an array
-            let newCourseType = [...courseType];
-
-            // Toggling between all and not all
-            if (newValue === "all" && courseType.indexOf("all") < 0) {
-                newCourseType = ["all"];
-            } else if (courseType[0] === "all" && newValue !== "all" && newValue !== "None") {
-                newCourseType = [newValue];
-            }
-            // Toggle between None and not None
-            else if (newValue === "None" && courseType.indexOf("None") < 0) {
-                newCourseType = ["None"];
-            } else if (courseType[0] === "None" && newValue !== "all" && newValue !== "None") {
-                newCourseType = [newValue];
-            }
-            // When it's not all and not none, remove value if it's exist, except we do all if we have nothing left
-            else if (newValue !== "all" && newValue !== "None" && courseType.indexOf(newValue) >= 0) {
-                // ECMA6 method to remove by value (all occurrences) in an array (it makes a copy without modifying original array)
-                const updatedCourseType = newCourseType.filter(e => e !== newValue);
-                console.log(updatedCourseType);
-                if (updatedCourseType[0] === "" || updatedCourseType.length === 0) {
-                    newCourseType = ["all"];
-                } else {
-                    newCourseType = updatedCourseType;
-                }
-            } else if (newValue !== "all" && newValue !== "None" && courseType.indexOf(newValue) < 0) {
-                // Non-Mutative way to concat an array
-                newCourseType = courseType.concat(newValue);
-            }
-            params.set('course_type', newCourseType.join("."));
-            window.history.pushState(null, null, '?' + params.toString());
-
-            setCourseType(newCourseType);
+            setStateFn(result);
         }
     }
 
@@ -332,7 +263,7 @@ async function fetchCourse(courseName= "all", courseLevel = ['all'], creditNumbe
 
     // Example backend search query:
     // You may assume that courseName, courseLevel, creditNumber, courseType are not null
-    // You may assume that no value will be repeated in a query, but (for query that can have multiple values) you may not assume that values are in sorted order. Hence, courseLevel=400.100.200.300 may occur.
+    // You may assume that no value will be repeated in a query, but (for query that can have multiple values) you may NOT assume that values are in sorted order. Hence, courseLevel=400.100.200.300 is a valid back-end query.
     // https://uwclassmate.com/api/search?courseName=cse142&courseLevel=all&creditNumber=1.4&courseType=DIV.IS
 
     let paramsObj = {courseName: courseName, level: courseLevel.join('.'), credit: creditNumber.join('.'), creditType: courseType.join('.')};
@@ -341,7 +272,7 @@ async function fetchCourse(courseName= "all", courseLevel = ['all'], creditNumbe
     searchParams.toString();
     const currentUrl = ServerConfig.SERVER_URL + "/api/search?" + searchParams.toString();
 
-    // Practice re-write this code using async/await
+    // Re-write this fetch code using async await
     try {
         // Display loading screen only if we don't have any course card displays previously
         if(courseCards.length === 0) {
@@ -379,7 +310,6 @@ async function fetchCourse(courseName= "all", courseLevel = ['all'], creditNumbe
             setLoadedCallBackFn(true);
             setCourseCardsCallBackFn(courseTemp);
         }
-
     } catch (err) {
         setLoadedCallBackFn(true);
         setErrorMessageCallBackFn(true);
@@ -387,49 +317,4 @@ async function fetchCourse(courseName= "all", courseLevel = ['all'], creditNumbe
         // Print out stack trace of this error on this webpage
         setErrorMessageCallBackFn(err.stack);
     }
-    //
-    // fetch(currentUrl)
-    //     .then(function (response) {
-    //         if (response.ok) {
-    //             // console.log(response);
-    //             if (response.status === 200) {
-    //                 return response.json()
-    //             }
-    //             if (response.status === 204) {
-    //                     setLoadedCallBackFn(true);
-    //                     setCourseCardsCallBackFn([]);
-    //                     setIsCourseDNECallBackFn(true);
-    //             } else {
-    //                 setCourseCardsCallBackFn([]);
-    //                 setErrorMessageCallBackFn(response.status);
-    //                 return {result: []};
-    //             }
-    //         } else {
-    //             setErrorMessageCallBackFn(response.status + ": " + response.statusText);
-    //             return Promise.reject(new Error(response.status + ": " + response.statusText))
-    //         }
-    //     })
-    //     .then(res => {
-    //         let courses = res.result;
-    //         let courseTemp = [];
-    //         for (let i = 0; i < courses.length; i++) {
-    //             let course = courses[i];
-    //             let curCreditType = course.creditType;
-    //             let curTag = null;
-    //             if (curCreditType !== null) {
-    //                 curTag = curCreditType.split("/");
-    //             }
-    //             courseTemp.push({
-    //                 courseName: course.courseName,
-    //                 courseDescription: course.courseFullName,
-    //                 tags: curTag,
-    //                 credit: course.credit[0]
-    //             });
-    //         }
-    //         setIsCourseDNECallBackFn(false);
-    //         setErrorMessageCallBackFn(false);
-    //         setLoadedCallBackFn(true);
-    //         setCourseCardsCallBackFn(courseTemp);
-    //     })
-    //     .catch(exception=>{setCourseCardsCallBackFn([]); setErrorMessageCallBackFn(exception.toString());});
 }
