a3bebce5c6ca6ec96522a00e3a21f1c8d38ac2d7
David Xie <xdkdavid@gmail.com>
2022-01-26 20:55:09 -0800

    add state setter functions

--- src/component/search/controller/SearchQuerySlice.js
+++ src/component/search/controller/SearchQuerySlice.js
@@ -1,35 +1,50 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+// Global constants
+const LEVELS = ["100", "200", "300", "400", "500"];
+const CREDITS = ["1", "2", "3", "4", "5", "5+"];
+// Took out None, since it's also a utility value (should be mutually exclusive selection)
+const CREDIT_TYPES = ["C", "DIV", "I&S", "NW", "QSR", "VLPA", "W"];
+
+function checkValue(currArr, referenceConst, extraAllowedValue=[]) {
+    if (currArr.includes("all")) {
+        return ["all"];
+    } else if (!currArr.every(r => referenceConst.concat(["all"].concat(extraAllowedValue)).includes(r))) {
+        return ["all"]; // default to all for malformed query
+    }
+    return currArr;
+}
+
 export const searchSlice = createSlice({
     name: 'search_param',
     initialState: {
-        value: 0
+        courseName: "all",
+        courseLevel: ["all"],
+        creditNumber: ["all"],
+        creditType: ["all"],
     },
     // Listen to state change event
     reducers: {
-        increment: state => { // Pass the state variable in when calling the state updater function
-            // Redux Toolkit allows us to write "mutating" logic in reducers. It
-            // doesn't actually mutate the state because it uses the Immer library,
-            // which detects changes to a "draft state" and produces a brand new
-            // immutable state based off those changes
-            state.value += 1
+        setCourseName: (state, action) => {
+            if(action.payload === undefined) {
+                state.courseName = "all";
+            }  else {
+                state.courseName = action.payload.replace(/[^a-zA-Z0-9]/gi, ""); // Only keep alphanumeric characters
+            }
         },
-        decrement: state => {
-            state.value -= 1
+        setCourseLevel: (state, action) => {
+            state.courseLevel = checkValue(action.payload, LEVELS);
         },
-        incrementByAmount: (state, action) => {
-            state.value += action.payload
+        setCreditNumber: (state, action) => {
+            state.creditNumber = checkValue(action.payload, CREDITS);
         },
-        resetCounter: state => {
-            state.value =0
-        },
-        doubleCounter: state => {
-            state.value *= 2
+        setCreditType: (state, action) => {
+            state.creditType = checkValue(action.payload, CREDIT_TYPES, ["IS", "None"]);
         }
     }
 })
 
 // Action creators are generated for each case reducer function
-export const { increment, decrement, incrementByAmount, resetCounter, doubleCounter } = searchSlice.actions
+export const { setCourseName, setCourseLevel, setCreditNumber, setCreditType } = searchSlice.actions
 
 export default searchSlice.reducer
