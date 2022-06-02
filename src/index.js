66c27d915cb88d3c695efcc862164d37fccb2e0a
Shangzhen Yang <shangzheny01@gmail.com>
2022-06-01 17:33:03 -0700

    Disable copying

--- src/index.js
+++ src/index.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import Routing from './component/routing/Route'
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Routing from "./component/routing/Route";
+import reportWebVitals from "./reportWebVitals";
 
 // David Xie: Add react-redux dependency for cross-component state management, which is needed for search bar communicate with search result page
-import { Provider } from 'react-redux'
-import store from './controller/ReduxStore'
-
+import { Provider } from "react-redux";
+import store from "./controller/ReduxStore";
 
 ReactDOM.render(
     // David Xie: Add react-redux dependency for cross-component state management
 
-      <React.StrictMode>
+    <React.StrictMode>
         <Provider store={store}>
-        <Routing />
+            <Routing />
         </Provider>
-      </React.StrictMode>
+    </React.StrictMode>
     ,
-  document.getElementById('root')
+    document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
@@ -27,20 +25,23 @@ ReactDOM.render(
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
+setTimeout(() => { // make sure that it will not be removed by extensions
+    document.oncopy = () => false;
+}, 1000);
 if (
-  navigator.userAgent.includes("MicroMessenger/") ||
-  navigator.userAgent.includes("QQ/")
+    navigator.userAgent.includes("MicroMessenger/") ||
+    navigator.userAgent.includes("QQ/")
 ) {
-  const newDiv = document.createElement("div");
-  const newTitle = document.createElement("h3");
-  const newUrlDiv = document.createElement("div");
-  const newImg = new Image();
-  newDiv.classList.add("full-screen");
-  newTitle.innerText = "如需浏览，请使用浏览器访问";
-  newUrlDiv.innerText = "https://uwclassmate.com/";
-  newImg.src = "https://cdn.jsdelivr.net/gh/kujian/weixinTip/live_weixin.png";
-  newDiv.appendChild(newTitle);
-  newDiv.appendChild(newUrlDiv);
-  newDiv.appendChild(newImg);
-  document.body.appendChild(newDiv);
+    const newDiv = document.createElement("div");
+    const newTitle = document.createElement("h3");
+    const newUrlDiv = document.createElement("div");
+    const newImg = new Image();
+    newDiv.classList.add("full-screen");
+    newTitle.innerText = "如需浏览，请使用浏览器访问";
+    newUrlDiv.innerText = "https://uwclassmate.com/";
+    newImg.src = "https://cdn.jsdelivr.net/gh/kujian/weixinTip/live_weixin.png";
+    newDiv.appendChild(newTitle);
+    newDiv.appendChild(newUrlDiv);
+    newDiv.appendChild(newImg);
+    document.body.appendChild(newDiv);
 }
