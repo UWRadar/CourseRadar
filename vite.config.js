4da571a665e1e0c76e0bb6a1b089fba92584e6c7
Shangzhen Yang <shangzheny01@gmail.com>
2023-01-16 00:09:55 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,17 @@ dns.setDefaultResultOrder("verbatim");
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks(id) {
+					if (id.includes("node_modules")) {
+						return "vendor";
+					}
+				}
+			}
+		}
+	},
 	css: {
 		postcss: {
 			plugins: [autoprefixer]
